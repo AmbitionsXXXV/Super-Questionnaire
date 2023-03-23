ca7fd79167054f224117f9672633000e8c96a3e4
etctera <yilonglei790@gmail.com>
2023-03-24 00:30:32 +0800

    chore: register page

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -32,10 +32,22 @@ const Register: FC = () => {
       </div>
       <div>
         <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} onFinish={onFinish}>
-          <Form.Item label="用户名" name="username">
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: "请输入用户名" },
+              { type: "string", min: 5, max: 20, message: "字符长度在 5-20 之间" },
+              { pattern: /^\w+$/, message: "只能是字母数字下划线" }
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="密码" name="password">
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: "请输入密码" }]}
+          >
             <Input.Password />
           </Form.Item>
           <Form.Item
