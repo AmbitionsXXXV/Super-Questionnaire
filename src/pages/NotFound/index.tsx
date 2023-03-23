e4852a749eac842555ba124952f917b722170e34
etctera <yilonglei790@gmail.com>
2023-03-23 15:23:52 +0800

    chore: 404page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,23 @@
+import { MANAGE_INDEX_PATHNAME } from "@/router";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+import type { FC } from "react";
+
+const NotFound: FC = () => {
+  const navigator = useNavigate();
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => navigator(MANAGE_INDEX_PATHNAME)}>
+          返回首页
+        </Button>
+      }
+    />
+  );
+};
+
+export default NotFound;
