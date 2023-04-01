4acbf48ba6883430e91bfa2076f966bd46aebf1f
etctera <yilonglei790@gmail.com>
2023-04-01 21:23:08 +0800

    chore: search hook

---
+++ src/hooks/useLoadQuestionListData.ts
@@ -0,0 +1,42 @@
+import {
+  LIST_PAGE_PARAM_KEY,
+  LIST_PAGE_SIZE,
+  LIST_PAGE_SIZE_PARAM_KEY,
+  LIST_SEARCH_PARAM_KEY
+} from "@/constant";
+import { getQuestionListService } from "@/service/question";
+import { useRequest } from "ahooks";
+import { useSearchParams } from "react-router-dom";
+
+type OptionType = {
+  isStar: boolean;
+  isDeleted: boolean;
+};
+
+function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
+  const { isStar, isDeleted } = opt;
+  const [searchParams] = useSearchParams();
+  const { data, loading, error } = useRequest(
+    async () => {
+      const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || "";
+      const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || "") || 1;
+      const pageSize =
+        parseInt(searchParams.get(LIST_PAGE_SIZE_PARAM_KEY) || "") || LIST_PAGE_SIZE;
+
+      return await getQuestionListService({
+        keyword,
+        isStar,
+        isDeleted,
+        page,
+        pageSize
+      });
+    },
+    {
+      refreshDeps: [searchParams]
+    }
+  );
+
+  return { data, loading, error };
+}
+
+export default useLoadQuestionListData;
