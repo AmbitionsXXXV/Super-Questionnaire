a950bd0411345ac32acbd83b4ce39e1a062d093c
etctera <yilonglei790@gmail.com>
2023-04-02 10:08:26 +0800

    chore: update useLoadQuestionData hook

--- src/hooks/useLoadQuestionListData.ts
+++ src/hooks/useLoadQuestionListData.ts
@@ -16,7 +16,7 @@ type OptionType = {
 function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
   const { isStar, isDeleted } = opt;
   const [searchParams] = useSearchParams();
-  const { data, loading, error } = useRequest(
+  const { data, loading, error, refresh } = useRequest(
     async () => {
       const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || "";
       const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || "") || 1;
@@ -36,7 +36,7 @@ function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
     }
   );
 
-  return { data, loading, error };
+  return { data, loading, error, refresh };
 }
 
 export default useLoadQuestionListData;
