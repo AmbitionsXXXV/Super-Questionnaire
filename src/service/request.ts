37e6d9b01c4aba72d50bef1f54e61671b4127f3b
etctera <yilonglei790@gmail.com>
2023-04-02 14:19:23 +0800

    chore: request interceptor

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,9 +1,19 @@
 import axios from "axios";
 import { message } from "antd";
+import { getToken } from "@/utils/user-token";
 
 const instance = axios.create({
   timeout: 10 * 1000
 });
+
+// request 拦截：每次请求都带上 token
+instance.interceptors.request.use(
+  config => {
+    config.headers["Authorization"] = `Bearer ${getToken()}`; // JWT 的固定格式
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
 // response interceptor: 处理 error 并结合 message 提示信息
 instance.interceptors.response.use(res => {
