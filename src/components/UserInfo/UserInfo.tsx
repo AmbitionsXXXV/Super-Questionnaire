824afda93c33791653214a7c4d9c68365c3b5e87
etctera <yilonglei790@gmail.com>
2023-04-02 22:19:08 +0800

    chore: userinfo component

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,16 +1,40 @@
 import { LOGIN_PATHNAME } from "@/router";
+import { getUserInfoService } from "@/service/user";
+import { removeToken } from "@/utils/user-token";
+import { UserOutlined } from "@ant-design/icons";
+import { useRequest } from "ahooks";
+import { Button, message } from "antd";
 import type { FC } from "react";
 import {} from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const UserInfo: FC = () => {
-  // Todo:已经登录显示内容
+  const navigator = useNavigate();
+  const { data } = useRequest(getUserInfoService);
+  const { username, nickname } = data ?? {};
 
-  return (
+  function logout() {
+    // dispatch(logoutReducer()); // 清空了 redux user 数据
+    removeToken(); // 清除 token 的存储
+    message.success("退出成功");
+    navigator(LOGIN_PATHNAME);
+  }
+
+  const UserInfo = (
     <>
-      <Link to={LOGIN_PATHNAME}>登陆</Link>
+      <span className="text-gray-300">
+        <UserOutlined />
+        {nickname}
+      </span>
+      <Button type="link" onClick={logout}>
+        退出
+      </Button>
     </>
   );
+
+  const Login = <Link to={LOGIN_PATHNAME}>登录</Link>;
+
+  return <div>{username ? UserInfo : Login}</div>;
 };
 
 export default UserInfo;
