db553ec1dd14ce7cbb6d790ab74235959372c1bb
etctera <yilonglei790@gmail.com>
2023-05-01 22:07:23 +0800

    feat: editCanvas component custom

--- src/pages/question/edit/components/EditCanvas/index.tsx
+++ src/pages/question/edit/components/EditCanvas/index.tsx
@@ -1,16 +1,27 @@
 import { Spin } from "antd";
 import type { FC } from "react";
-import QuestionInput from "@/components/QuestionComponents/QuestionInput/component";
-import QuestionTitle from "@/components/QuestionComponents/QuestionTitle/component";
 import useGetComponentInfo from "@/hooks/useGetComponentInfo";
+import { ComponentsInfoType } from "@/store/modules/components";
+import { getComponentConfByType } from "@/components/QuestionComponents";
+
+// import QuestionInput from "@/components/QuestionComponents/QuestionInput/component";
+// import QuestionTitle from "@/components/QuestionComponents/QuestionTitle/component";
 
 type PropsType = {
   loading: boolean;
 };
 
+function genComponent(componentInfo: ComponentsInfoType) {
+  const { type, props } = componentInfo;
+  const componentConf = getComponentConfByType(type);
+  if (componentConf == null) return null;
+  const { Component } = componentConf;
+
+  return <Component {...props} />;
+}
+
 const EditCanvas: FC<PropsType> = ({ loading }) => {
   const { componentList } = useGetComponentInfo();
-  console.log(componentList);
 
   if (loading) {
     return (
@@ -22,18 +33,29 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
 
   return (
     <div className="bg-white min-h-full overflow-hidden">
-      <div className="m-3 border border-solid border-white p-3 rounded hover:border-slate-300">
-        {/* 屏蔽鼠标点击行为 */}
-        <div className="pointer-events-none">
+      {componentList.map(c => {
+        const { fe_id } = c;
+        return (
+          <div
+            key={fe_id}
+            className="m-3 border border-solid border-white p-3 rounded hover:border-slate-300"
+          >
+            <div className="pointer-events-none">{genComponent(c)}</div>
+          </div>
+        );
+      })}
+      {/* <div className="m-3 border border-solid border-white p-3 rounded hover:border-slate-300"> */}
+      {/* 屏蔽鼠标点击行为 */}
+      {/* <div className="pointer-events-none">
           <QuestionTitle />
         </div>
       </div>
-      <div className="m-3 border border-solid border-white p-3 rounded hover:border-slate-300">
-        {/* 屏蔽鼠标点击行为 */}
-        <div className="pointer-events-none">
+      <div className="m-3 border border-solid border-white p-3 rounded hover:border-slate-300"> */}
+      {/* 屏蔽鼠标点击行为 */}
+      {/* <div className="pointer-events-none">
           <QuestionInput />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
