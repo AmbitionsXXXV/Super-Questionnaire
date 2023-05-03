4252bc5e37f155c3dd865cfed067e21a247f9d52
etctera <yilonglei790@gmail.com>
2023-05-03 22:08:04 +0800

    feat: edit page left panel module

--- src/pages/question/edit/components/ComponentLib/index.tsx
+++ src/pages/question/edit/components/ComponentLib/index.tsx
@@ -1,8 +1,26 @@
-import { componentConfGroup } from "@/components/QuestionComponents";
+import {
+  ComponentConfType,
+  componentConfGroup
+} from "@/components/QuestionComponents";
 import { Typography } from "antd";
 import type { FC } from "react";
 
 const { Title } = Typography;
+
+function genComponent(c: ComponentConfType) {
+  const { title, type, Component, defaultProps } = c;
+
+  return (
+    <div
+      key={type}
+      className="mb-3 cursor-pointer bg-slate-100 border border-solid border-slate-200 p-3 rounded hover:border-slate-400"
+    >
+      <div className="pointer-events-none">
+        <Component />
+      </div>
+    </div>
+  );
+}
 
 const ComponentLib: FC = () => {
   return (
@@ -18,7 +36,7 @@ const ComponentLib: FC = () => {
             >
               {groupName}
             </Title>
-            {/* <div>{components.map(c => genComponent(c))}</div> */}
+            <div>{components.map(c => genComponent(c))}</div>
           </div>
         );
       })}
