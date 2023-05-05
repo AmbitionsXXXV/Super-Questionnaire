80d2e73848b8e4e42154ef7fc1ecd307a4e66613
etctera <yilonglei790@gmail.com>
2023-05-05 16:07:43 +0800

    feat: edit header's toolbox

---
+++ src/pages/question/edit/components/EditToolbox/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { Button, Space, Tooltip } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import { removeSelectedComponent } from "@/store/modules/components";
+
+const EditToolbox: FC = () => {
+  const dispatch = useDispatch();
+
+  function handleDelete() {
+    dispatch(removeSelectedComponent());
+  }
+
+  return (
+    <Space>
+      <Tooltip title="删除">
+        <Button shape="circle" icon={<DeleteOutlined />} onClick={handleDelete} />
+      </Tooltip>
+    </Space>
+  );
+};
+
+export default EditToolbox;
