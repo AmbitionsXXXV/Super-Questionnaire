30145ce4344e5a5406e442e771a31759ab680c71
etctera <yilonglei790@gmail.com>
2023-05-10 21:49:13 +0800

    feat: component isHidden

--- src/pages/question/edit/components/EditToolbar/index.tsx
+++ src/pages/question/edit/components/EditToolbar/index.tsx
@@ -1,20 +1,36 @@
 import type { FC } from "react";
 import { Button, Space, Tooltip } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EyeInvisibleOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
-import { removeSelectedComponent } from "@/store/modules/components";
+import {
+  changeComponentHidden,
+  removeSelectedComponent
+} from "@/store/modules/components";
+import UseGetComponentInfo from "@/hooks/useGetComponentInfo";
 
 const EditToolbox: FC = () => {
   const dispatch = useDispatch();
+  const { selectedId } = UseGetComponentInfo();
 
   function handleDelete() {
     dispatch(removeSelectedComponent());
+  }
+
+  function handleHidden() {
+    dispatch(changeComponentHidden({ fe_id: selectedId, isHidden: true }));
   }
 
   return (
     <Space>
       <Tooltip title="删除">
         <Button shape="circle" icon={<DeleteOutlined />} onClick={handleDelete} />
+      </Tooltip>
+      <Tooltip title="隐藏">
+        <Button
+          shape="circle"
+          icon={<EyeInvisibleOutlined />}
+          onClick={handleHidden}
+        />
       </Tooltip>
     </Space>
   );
--- src/store/modules/components.ts
+++ src/store/modules/components.ts
@@ -7,6 +7,7 @@ export type ComponentsInfoType = {
   fe_id: string; // Todo
   type: string;
   title: string;
+  isHidden?: boolean;
   props: ComponentPropsType;
 };
 
@@ -84,7 +85,31 @@ export const componentsSlice = createSlice({
       const index = componentList.findIndex(c => c.fe_id === removeId);
 
       componentList.splice(index, 1);
-    })
+    }),
+
+    // 隐藏/显示组件
+    changeComponentHidden: produce(
+      (
+        draft: ComponentsStateType,
+        { payload }: PayloadAction<{ fe_id: string; isHidden: boolean }>
+      ) => {
+        const { componentList = [] } = draft;
+        const { fe_id, isHidden } = payload;
+
+        let newSelectedId: string;
+        if (isHidden) {
+          // 隐藏
+          newSelectedId = getNextSelectedId(fe_id, componentList);
+        } else {
+          // 显示
+          newSelectedId = fe_id;
+        }
+        draft.selectedId = newSelectedId;
+
+        const curComp = componentList.find(c => c.fe_id === fe_id);
+        if (curComp) curComp.isHidden = isHidden;
+      }
+    )
   }
 });
 
@@ -93,6 +118,7 @@ export const {
   changeSelectedId,
   addComponent,
   changeComponentProps,
-  removeSelectedComponent
+  removeSelectedComponent,
+  changeComponentHidden
 } = componentsSlice.actions;
 export default componentsSlice.reducer;
